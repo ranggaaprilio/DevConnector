ec1fc3da73379786f294070574b3fb53aeb64e08
rangga <rangga@gmail.com>
2021-03-06 13:57:51 +0700

    deploy setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path =requeire('path');
 
 //Connect DB
 connectDB();
@@ -9,13 +10,23 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // root Route
-app.get("/", (req, res) => res.send("Server is Already Running"));
+// app.get("/", (req, res) => res.send("Server is Already Running"));
 
 // define route
 app.use("/api/users", require("./Routes/API/users"));
 app.use("/api/auth", require("./Routes/API/auth"));
 app.use("/api/posts", require("./Routes/API/post"));
 app.use("/api/profile", require("./Routes/API/profile"));
+
+//serve static asset on production
+
+if(process.env.NODE_ENV==='production'){
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0';
